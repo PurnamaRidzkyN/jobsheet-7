cc90557b4754c79d426e711a539481384c445a8d
PurnamaRidzkyN <purnamanugraha492@gmail.com>
2023-10-12 14:06:50 +0700

    penambahan file whilegaji23

--- WhileGaji23.java
+++ WhileGaji23.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class WhileGaji23 {
+
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        int jumlahKaryawan ;
+        int jumlahJamLembur ;
+        double gajiLembur  = 0;
+        double totalGajiLembur = 0;
+        String jabatan ;
+
+        System.out.print("Masukan jumlah karyawan : ");
+        jumlahKaryawan = scan.nextInt();
+
+        int i = 0;
+        while (i < jumlahKaryawan) {
+        System.out.println("Pilihan jabatan - direktur, manajer, karyawan");
+        System.out.print("Masukan jabatan karyawan ke-" + (i+1) + " : " );
+        jabatan = scan.next();
+        System.out.print("Masukan jumlah jam lembur: ");
+        jumlahJamLembur = scan.nextInt();
+        i++;
+
+        if ( jabatan.equalsIgnoreCase("direktur")){
+            continue;
+        }else if ( jabatan.equalsIgnoreCase("manajer")){
+            gajiLembur = jumlahJamLembur * 100000;
+        }else if ( jabatan.equalsIgnoreCase("karyawan")){
+            gajiLembur = jumlahJamLembur * 75000;
+        }else {
+            System.out.println("Jabatan invalid");
+            i--;
+        }
+         totalGajiLembur += gajiLembur;  
+        }
+         System.out.println("Total Gaji Lembur : " + totalGajiLembur);
+
     }
 }
